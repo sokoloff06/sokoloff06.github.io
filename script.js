2fd0ad29c32ee7f123405bf4052be9c383e2cf70
Vitaly Sokolov <sokoloff06@gmail.com>
2022-12-07 14:22:37 +0200

    Add long tasks experiments

---
+++ script.js
@@ -0,0 +1,13 @@
+function runLongTask() {
+  var startTime = Date.now();
+  console.log("Start Long Task");
+  array = new Array(100000000);
+  for (let index = 0; index < array.length; index++) {
+    array[index] = index;
+  }
+  console.log(`Finish Long Task that took ${Date.now() - startTime}ms`);
+}
+
+function pushEvent(eventName) {
+  dataLayer.push({ event: eventName });
+}
